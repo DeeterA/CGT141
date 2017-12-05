63a6b4bdcb6183e4605aa2ebe5bbdf876dbf5123
DeeterA <deetera@purdue.edu>
2017-12-05 15:33:33 -0500

    Stylized contact form

--- doContact.php
+++ doContact.php
@@ -1,17 +1,30 @@
 <?php
+    session_start();
+
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $fullName = $firstName." ".$lastName;
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
+    $message = wordwrap($message, 70, "\r\n");
     $from = 'From: TKE Website';
     $to = 'andrewdeeter.ad@gmail.com';
 
-    $body = "From: $fullName\n E-Mail: $email\n Message:\n $message";
+    $emailFrom = $fullName."<".$email.">";
+    $headers = "From: ".$emailFrom." \r\n";
+    $headers .= "Reply-To: ".$emailFrom." \r\n";
 
-    mail($to, $subject, $body, $from);
-    header("Location:contactUs.html");
-    exit;
+    $response = mail('andrewdeeter.ad@gmail.com', $subject, $message, $headers);
+    
+    if ($response == true) {
+        $_SESSION["responseMessage"] = "You've successfully sent us a message, we'll be in touch soon!";
+        header("Location:contactUs.html");
+        exit;
+    } else {
+        $_SESSION["responseMessage"] = "There's been an error";
+        header("Location:contactUs.html");
+        exit;
+    }
 
  ?>
